db0d965763f3b074f75b2df21098cc62f32f8102
Rob C <robcabardo@gmail.com>
2015-05-02 11:37:05 -0500

    Initial commit

---
+++ CharacterArray/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+                                          'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            
+            int vowels = countVowels(alphabet);
+            int consonants = countConsonants(alphabet);
+
+            Console.WriteLine("There are " + vowels + " vowels in the array.");
+            Console.WriteLine("There are " + consonants + " consonants in the array.");
+            Console.ReadLine();
+            
+        }
+        static int countVowels(char[] alphabet)
+        {
+            int i = 0;
+
+            foreach(char val in alphabet)
+            {
+                if (val == 'a' || val == 'e' || val == 'i' || val == 'o' || val == 'u')
+                {
+                    i++;
+                }
+            }
+            return i;
+        }
+        static int countConsonants(char[] alphabet)
+        {
+            int i = 0;
+
+            foreach (char val in alphabet)
+            {
+                if (val != 'a' && val != 'e' && val != 'i' && val != 'o' && val != 'u')
+                {
+                    i++;
+                }
+            }
+            return i;
+        }
+    }
+}
---
+++ StudentScores/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentScores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many scores do you have? ");
+            int numberOfScores = Convert.ToInt32(Console.ReadLine());
+
+            int[] actualScores = new int[numberOfScores];
+            
+            for(int i = 0; i < numberOfScores; i++)
+            {
+                Console.Write("Enter a score: ");
+                actualScores[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            formula(numberOfScores, actualScores);
+
+            Console.ReadLine();
+        }
+        static void formula(int numberOfScores, int[] actualScores)
+        {
+            int highestNum = 0;
+            int lowestNum = actualScores[0];
+            int sum = 0;
+
+            for (int i = 0; i < numberOfScores; i++)
+            {
+                if (actualScores[i] > highestNum)
+                {
+                    highestNum = actualScores[i];
+                }
+                else lowestNum = actualScores[i];
+                sum = sum + actualScores[i];
+            }
+
+        Console.WriteLine("The highest number: " + highestNum);
+        Console.WriteLine("The lowest number: " + lowestNum);
+        Console.WriteLine("The average: " + sum / numberOfScores);
+        }
+    }
+}
